ea80dbe38c420d9f60f3d3fe7c42d2ae40a46035
maaaashi <doyaseizin.7@gmail.com>
2023-11-08 21:48:48 +0900

    環境起動まで確認

--- api/cargo-build.sh
+++ api/cargo-build.sh
@@ -2,11 +2,4 @@
 
 rustup default nightly
 rustup update
-cargo install cargo-edit
-cargo add axum
-cargo add tokio -F full
-cargo add chrono -F serde
-cargo add serde -F derive
-cargo add serde_json
-cargo add uuid -F "v4 serde"
-cargo add tower-http -F "cors"
+cargo install --path .
